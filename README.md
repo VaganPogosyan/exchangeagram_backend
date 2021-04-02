ddf718382661b649f02b9a2a20e83967f07659b7
Vagan Pogosyan <vagan2502@gmail.com>
2021-04-02 01:51:14 -0400

    created MySQL database and connected it to server

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Initial connection to Github
 
 Set up dev branch
+
+
+
+### Resources:
+
+https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+// run MySQL in terminal === /usr/local/mysql/bin/mysql -u root -p
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const mysql = require('mysql');
-const apiRouter = require('./controllers/index')
+const apiRouter = require('./routes/index')
+
+// Configuration for connection to MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Vkontakte1',
+
+});
 
 const APP = express();
 const PORT = 3000;
 
 APP.use('/api/exchangeagram', apiRouter)
 APP.use(express.json());
+
+// Connect to MySQL (throw an error if there is one, if not console.log)
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL is connected')
+})
+
+// Create db
+APP.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE exchangeagram';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database exchangeagram created');
+    })
+});
+
+
+
 
 APP.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`)
